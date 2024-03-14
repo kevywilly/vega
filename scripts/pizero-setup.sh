a141dc6c320ab562608e36c0db8331f38548df81
kevywilly <kevywilly@gmail.com>
2024-03-14 00:42:11 -0400

    fix kinematics

---
+++ scripts/pizero-setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Configure Swap
+# sudo dphys-swapfile swapoff
+# sudo vi /etc/dphys-swapfile
+# CONF_SWAPSIZE=512
+# sudo dphys-swapfile setup
+# sudo dphys-swapfile swapon
+
+# System dependencies
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y python3-pip \
+python3-venv \
+python3-pil
+
+# Adafruit Dependencies
+sudo apt install -y --upgrade python3-setuptools \
+i2c-tools \
+libgpiod-dev \
+libcap-dev
+python3-libgpiod \
+python3-libcamera \
+python3-picamera2 \
+python3-opencv \
+opencv-data
+
+# setup python virtual environment
+#python3 -m venv ~/.robotics
+#source ~/.robotics/bin/activate
+#echo 'source ~/.robotics/bin/activate' >> ~/.bashrc
+
+# python packages
+yes | pip3 install --upgrade Rpi.gpio \
+adafruit-blinka \
+Adafruit_CircuitPython_BNO055 \
+pillow \
+numpy \
+traitlets \
+opencv-python
+
+
+
+
+
