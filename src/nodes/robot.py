abb79976de3555cd8f6162f94b50919aca21f130
kevywilly <kevywilly@gmail.com>
2024-06-27 03:12:19 +0100

    deploy

--- src/nodes/robot.py
+++ src/nodes/robot.py
@@ -80,7 +80,8 @@ class Robot(Node):
         self._start_nodes()
         self._setup_subscriptions()
         time.sleep(0.2)
-        self.z_level()
+        self.yaw_level()
+        self.pitch_level()
 
         self.loaded()
 
