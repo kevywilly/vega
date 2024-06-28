a080f17b86aa4c88d615d16c695c69b736d4c3b6
kevywilly <kevywilly@gmail.com>
2024-06-28 22:22:52 +0100

    deploy

--- src/nodes/robot.py
+++ src/nodes/robot.py
@@ -133,37 +133,36 @@ class Robot(Node):
 
     def process_move(self, move_type: MoveTypes):
 
-        if not self.move_type == move_type:
-            if move_type == MoveTypes.FORWARD:
-                self.gait = Trot(
-                    p0=settings.position_ready + settings.position_forward_offsets,
-                    **settings.trot_params
-                )
-            elif move_type == MoveTypes.FORWARD_LT:
-                self.gait = Trot(**settings.turn_params, turn_pct=0.7)
-            elif move_type == MoveTypes.FORWARD_RT:
-                self.gait = Trot(**settings.turn_params, turn_pct=-0.7)
-            elif move_type == MoveTypes.BACKWARD:
-                self.gait = Trot(
-                    p0=settings.position_ready + settings.position_backward_offsets,
-                    **settings.trot_params,
-                    reversed=True
-                )
-            elif move_type == MoveTypes.BACKWARD_LT:
-                self.gait = Trot(**settings.turn_params, turn_pct=0.7, reversed=True)
-            elif move_type == MoveTypes.BACKWARD_RT:
-                self.gait = Trot(**settings.turn_params, turn_pct=-0.7, reversed=True)
-            elif move_type == MoveTypes.LEFT:
-                self.gait = Sidestep(**settings.sidestep_params, reversed=True)
-            elif move_type == MoveTypes.RIGHT:
-                self.gait = Sidestep(**settings.sidestep_params)
-            elif move_type == MoveTypes.STOP:
-                self.stop()
+        if move_type == MoveTypes.FORWARD:
+            self.gait = Trot(
+                p0=settings.position_ready + settings.position_forward_offsets,
+                **settings.trot_params
+            )
+        elif move_type == MoveTypes.FORWARD_LT:
+            self.gait = Trot(**settings.turn_params, turn_pct=0.7)
+        elif move_type == MoveTypes.FORWARD_RT:
+            self.gait = Trot(**settings.turn_params, turn_pct=-0.7)
+        elif move_type == MoveTypes.BACKWARD:
+            self.gait = Trot(
+                p0=settings.position_ready + settings.position_backward_offsets,
+                **settings.trot_params,
+                reversed=True
+            )
+        elif move_type == MoveTypes.BACKWARD_LT:
+            self.gait = Trot(**settings.turn_params, turn_pct=0.7, reversed=True)
+        elif move_type == MoveTypes.BACKWARD_RT:
+            self.gait = Trot(**settings.turn_params, turn_pct=-0.7, reversed=True)
+        elif move_type == MoveTypes.LEFT:
+            self.gait = Sidestep(**settings.sidestep_params, reversed=True)
+        elif move_type == MoveTypes.RIGHT:
+            self.gait = Sidestep(**settings.sidestep_params)
+        elif move_type == MoveTypes.STOP:
+            self.stop()
 
-            self.move_type = move_type
+        self.move_type = move_type
 
-            if self.move_type != MoveTypes.STOP:
-                self.moving = True
+        if self.move_type != MoveTypes.STOP:
+            self.moving = True
             
         return {
             "moving": self.moving,
