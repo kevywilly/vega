e3b72b4d5c9ac4abb81189bdd5f6bf2c1c85e6d7
kevywilly <kevywilly@gmail.com>
2024-06-29 13:57:15 +0100

    deploy

--- src/motion/gaits/gait.py
+++ src/motion/gaits/gait.py
@@ -63,7 +63,7 @@ class Gait(ABC):
         pass
 
     def get_positions(self, phase: int = 0, index: int = 0):
-        offsets = np.array([self.steps1[index], self.steps2[index], self.steps1[index], self.steps2[index]])
+        offsets = np.array([self.steps1[index], self.steps2[index], self.steps1[index], self.steps2[index]]).astype(float)
 
         if self.turn_pct is not None:
             f = 1-abs(self.turn_pct)
@@ -73,9 +73,9 @@ class Gait(ABC):
                 offsets[:, 0] *= [f, 1.0, 1.0, f]
 
         if phase == 0:
-            pos = (self.p0 + offsets)
+            pos = (self.p0 + offsets.astype(int))
         else:
-            pos = (self.p0 + np.roll(offsets, 1, 0))
+            pos = (self.p0 + np.roll(offsets.astype(int), 1, 0))
 
         return pos
 
