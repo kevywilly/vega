784051c03d526e6b7237f1cb5b7558c32f9d3d20
kevywilly <kevywilly@gmail.com>
2024-06-29 03:49:09 +0100

    deploy

--- settings.py
+++ settings.py
@@ -127,7 +127,7 @@ class Settings:
     trot_params: Dict[str, int] = {"stride": 50, "clearance": 65, "step_size": 15}
     trot_reverse_params: Dict[str, int] = {"stride": -40, "clearance": 60, "step_size": 15}
     sidestep_params: Dict[str, int] = {"stride": 25, "clearance": 30, "step_size": 15}
-    turn_params: Dict[str, int] = {"stride": 20, "clearance": 60, "step_size": 15}
+    turn_params: Dict[str, int] = {"stride": 10, "clearance": 60, "step_size": 15}
 
     def adjust_offsets(self, x: int = 0, y: int = 0, z: int = 0, group=None):
         if group is None:
