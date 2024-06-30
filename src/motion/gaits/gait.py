9cbedb7631e0bce095472a364c4537ce0f2d7aeb
kevywilly <kevywilly@gmail.com>
2024-06-30 23:20:43 +0100

    deploy

--- src/motion/gaits/gait.py
+++ src/motion/gaits/gait.py
@@ -85,7 +85,7 @@ class Gait(ABC):
             return offsets * [tf, 1.0, 1.0, tf]
 
     def get_positions(self, phase: int = 0, index: int = 0):
-        return self.p0 + self.get_offsets()
+        return self.p0 + self.get_offsets(index)
 
     def step_generator(self):
         """
