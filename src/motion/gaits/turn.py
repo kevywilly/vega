69ab4344cc8ade9edb93abb1cffcc7fd7b12e91b
kevywilly <kevywilly@gmail.com>
2024-07-02 22:31:07 +0100

    deploy

--- src/motion/gaits/turn.py
+++ src/motion/gaits/turn.py
@@ -22,7 +22,7 @@ class Turn(Gait):
             np.zeros(self.num_steps*3) + 1,
             np.cos(np.radians(np.linspace(0, 90, self.num_steps))),
             np.zeros(self.num_steps*3),
-        ]) * -1 * mag_y
+        ]) * -1 * mag_y * self.turn_direction
 
         z = np.hstack([
             np.sin(np.radians(np.linspace(0, 180, self.num_steps))),
@@ -40,4 +40,58 @@ class Turn(Gait):
         self.steps2 = np.roll(front, self.num_steps, axis=0)
         self.steps3 = np.roll(back, self.num_steps*6, axis=0)
         self.steps4 = np.roll(back, self.num_steps*7, axis=0)
+
+from abc import ABC, abstractmethod
+from typing import Optional
+
+import numpy as np
+
+from settings import settings
+from src.motion.gaits.gait import Gait
+
+
+
+
+
+class Turn2(Gait):
+    def build_steps(self):
+        mag_z = -self.clearance
+        mag_y = self.stride
+
+        x = np.zeros(self.num_steps * 8)
+        step = np.sin(np.radians(np.linspace(0, 90, self.num_steps))) * mag_y * self.turn_direction
+        back = np.cos(np.radians(np.linspace(0, 90, self.num_steps))) * mag_y * self.turn_direction
+        up_down = np.sin(np.radians(np.linspace(0, 180, self.num_steps))) * mag_z,
+        stepped = np.zeros(self.num_steps) + 1
+        zero = np.zeros(self.num_steps)
+
+        self.steps1 = np.vstack([
+            [zero, step, up_down],
+            [zero, stepped, zero],
+            [zero, stepped, zero],
+            [zero, stepped, zero],
+            [zero, back, zero]
+        ])
+        self.steps2 = np.vstack([
+            [zero, zero, zero],
+            [zero, step, up_down],
+            [zero, stepped, zero],
+            [zero, stepped, zero],
+            [zero, back, zero]
+        ])
+        self.steps3 = np.vstack([
+            [zero, zero, zero],
+            [zero, zero, zero],
+            [zero, -step, up_down],
+            [zero, stepped, zero],
+            [zero, back, zero]
+        ])
+        self.steps4 = np.vstack([
+            [zero, zero, zero],
+            [zero, zero, zero],
+            [zero, zero, zero],
+            [zero, -step, up_down],
+            [zero, back, zero]
+        ])
+
 
