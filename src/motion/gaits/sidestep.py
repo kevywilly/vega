59ff6e3827729a303fb8bdec51f28020d5f1dc8d
kevywilly <kevywilly@gmail.com>
2024-07-05 14:56:53 +0100

    deploy

--- src/motion/gaits/sidestep.py
+++ src/motion/gaits/sidestep.py
@@ -8,16 +8,15 @@ class Sidestep(Gait):
         mag_z = -self.clearance
         mag_y = self.stride
 
-        x = np.repeat(self.zeros,3)
+        x = np.repeat(self.zeros,2)
         y = np.hstack([
             self.stride_forward(),
-            self.stride_front_to_back(self.num_steps*2),
+            self.stride_front_to_back(),
         ]) * mag_y
         z = np.hstack([
             self.updown(),
-            self.zeros,
             self.zeros
         ]) * mag_z
 
-        self.steps1 = self.reshape_steps(np.array([x, y, z]), self.num_steps * 3)
+        self.steps1 = self.reshape_steps(np.array([x, y, z]), self.num_steps * 2)
         self.steps2 = np.roll(self.steps1, self.num_steps, axis=0)
